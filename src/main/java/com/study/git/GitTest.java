7a88382b4a2f2463f7dc1ff563d96720285c6b3f
shiyue <1434075209@qq.com>
2023-04-02 14:16:31 +0800

    first commit

---
+++ src/main/java/com/study/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.study.git;
+
+/**
+ * @ClassName: GitTest
+ * @Author: shiyue_L
+ * @Date: 2023/4/2 13:51
+ * @Version 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+
+}
